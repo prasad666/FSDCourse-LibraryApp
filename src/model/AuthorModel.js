975d0dd5c28afd9014481fcb9455229b6e47928e
Prasad P N <prasad.p@dalason.com>
2022-08-08 21:30:07 +0530

    From

--- src/model/AuthorModel.js
+++ src/model/AuthorModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://libraryuser:7b53HnRn9J9Zq6a@cluster0.yz2ioso.mongodb.net/?retryWrites=true&w=majority"
+  "mongodb+srv://libraryuser:7b53HnRn9J9Zq6a@cluster0.yz2ioso.mongodb.net/LibraryApp2?retryWrites=true&w=majority"
 );
 const Schema = mongoose.Schema;
 
