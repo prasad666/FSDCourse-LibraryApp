6dfc6b68223774da3b2a7a9ae7df79249aec5088
Prasad P N <prasad.p@dalason.com>
2022-08-08 20:05:22 +0530

    Revert "Revert "Added routes for signup and login""
    This reverts commit 5a5f6244105daa7937bbc38ba92c475c6a361fe0.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const chalk = require("chalk");
 const path = require("path");
-const bodyparser = require("body-parser");
+
 const cors = require("cors");
 const dotenv = require("dotenv");
 
